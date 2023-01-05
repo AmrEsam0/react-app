a1b8c83ad249bd51dbf864c9703b12796a4bf475
AmrEssam <amr_esam@ymail.com>
2023-01-05 23:58:09 +0200

    adding the bones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import Click from './components/Click';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <Click />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Form />
+		</div>
+	);
 }
 
 export default App;
--- src/components/HashList.js
+++ src/components/HashList.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 function Names() {
-	return <div>Names</div>;
+	const names = ["Dareen", "Dalia", "Amr"].map((name) => <h2>{name}</h2>);
+	return <div>{names}</div>;
 }
 
 export default Names;
