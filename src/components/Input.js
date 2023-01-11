3e0a45d296697766ba7f812b336d00a5c893a9d6
Amr Essam <amr_esam@ymail.com>
2023-01-11 20:06:21 +0200

    More refs

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 
-class RefsDemo extends Component {
+class Input extends Component {
 	constructor(props) {
 		super(props);
 		this.inputRef = React.createRef();
 	}
 
-	clickHandler = () => {
-		alert(this.inputRef.current.value);
-	};
-
+	focusInput() {
+		this.inputRef.current.focus();
+	}
 	render() {
 		return (
 			<div>
 				<input type="text" ref={this.inputRef} />
-				<button onClick={this.clickHandler}>Click</button>
 			</div>
 		);
 	}
 }
 
-export default RefsDemo;
+export default Input;
